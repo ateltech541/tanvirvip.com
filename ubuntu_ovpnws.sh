36a583d1d06b21079128b7e0f38a46d061ed0796
ateltech541 <104555534+ateltech541@users.noreply.github.com>
2022-07-11 15:48:23 +0300

    Update ubuntu_ovpnws.sh

--- ubuntu_ovpnws.sh
+++ ubuntu_ovpnws.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-HOST='174.138.184.18';
-USER='ponyvipn_panel';
+HOST='66.45.248.110';
+USER='tanvirvi_tanvir';
 PASS='@@Atel123@@';
-DBNAME='ponyvipn_panel';
+DBNAME='tanvirvi_tanvir';
 
 install_require()
 {
